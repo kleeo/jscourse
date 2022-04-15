5f676d1432283ddbebe59eb86e108da61e0d9474
Dzmitry Klimau <Dzmitry_Klimau@epam.com>
2022-04-15 17:21:06 +0200

    Update blackjack

--- 3.blackjack/index.js
+++ 3.blackjack/index.js
@@ -1,21 +1,39 @@
-let cardOne = 2;
-let cardTwo = 1+1;
+let cardOne = 9;
+let cardTwo = 11;
 let sum = cardOne + cardTwo;
 let hasBlackjack = false;
 let isAlive = true;
+
 let continueMessage = "Do you want to draw a new card?";
 let victoryMessage = "You've won!";
-let failureMessage = "You've lost! :(";
+let failureMessage = "You've lost!";
 let message = "";
 
-if (sum <= 20) {
-    message = continueMessage;
-} else if (sum === 21) {
-    message = victoryMessage;
-    hasBlackjack = true;
-} else {
-    message = failureMessage;
-    isAlive = false;
+let messageElement = document.getElementById("message");
+let sumElement = document.getElementById("sum");
+let cardsElement = document.getElementById("cards");
+let newCardButton= document.getElementById("newCardBtn");
+
+function startGame() {
+    sumElement.textContent = "Sum: " + sum;
+    cardsElement.textContent = "Cards: " + cardOne + ", " + cardTwo;
+    if (sum <= 20) {
+        message = continueMessage;
+    } else if (sum === 21) {
+        message = victoryMessage;
+        hasBlackjack = true;
+    } else {
+        message = failureMessage;
+        isAlive = false;
+        newCardButton.setAttribute("disabled", "disabled");
+        newCardButton.setAttribute("class", "grayed");
+    }
+
+    messageElement.textContent = message;
 }
 
-console.log(message);
+function newCard() {
+    let card = 7;
+    sum += card;
+    startGame();
+}
